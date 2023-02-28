a40aa716533fe5809ba0677404fd022c39dd93e1
Douile <25043847+Douile@users.noreply.github.com>
2023-02-28 09:38:22 +0000

    Complete message_thread.c

--- message_thread.c
+++ message_thread.c
@@ -1,15 +1,25 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <pthread.h>
 
+#define THREAD_COUNT 10
+
 void* helloThread(void* thread_value) {
-	printf("Hello Thread! value = %d\n", (int)thread_value);
+	printf("Hello Thread! value = %d\n", (int) thread_value);
 	return NULL;
 }
 
 int main() {
-	pthread_t ID; /*pthread_t is the data type used to uniquely identify a thread*/
-	int value = 1;
-	pthread_create(&ID, NULL, helloThread, (void*)value);
-	pthread_exit(NULL); /*this function is called after a thread has completed and no longer required.*/
+  pthread_t threads[THREAD_COUNT];
+  printf("With join\n");
+  for (size_t i=0;i<THREAD_COUNT;i++) {
+    pthread_create(&threads[i], NULL, helloThread, (void *)i);
+  }
+
+  printf("No join\n");
+  for (size_t i=0;i<THREAD_COUNT;i++) {
+    pthread_create(&threads[i], NULL, helloThread, (void *)i);
+  }
+  pthread_exit(NULL); /*this function is called after a thread has completed and no longer required.*/
 	return 0;
 }
